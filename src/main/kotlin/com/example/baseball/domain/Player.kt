a2d761d2b9499ffbbaef1580d4c34e3a1649d695
ozaki <fs65ugumori@yahoo.co.jp>
2018-12-31 00:14:46 +0900

    add bean validation

--- src/main/kotlin/com/example/baseball/domain/Player.kt
+++ src/main/kotlin/com/example/baseball/domain/Player.kt
@@ -3,14 +3,20 @@ package com.example.baseball.domain
 import javax.persistence.Entity
 import javax.persistence.GeneratedValue
 import javax.persistence.Id
+import javax.validation.constraints.*
 
 @Entity
 data class Player(
         @Id
         @GeneratedValue
         val id: Long? = null,
+        @get:NotEmpty
         val name: String? = null,
+        @get:NotNull
+        @get:Min(value = 0)
+        @get:Max(value = 150)
         val age: Int? = null,
+        @get:Size(max = 20)
         val team: String? = null,
         val position: String? = null
 )
